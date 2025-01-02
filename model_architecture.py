2dfb2a03aa6aa7a1136142cfdb30b5b72e012738
AmanK-tech <amankhatri216@gmail.com.com>
2025-01-02 15:17:19 +0000

    model_architecture

---
+++ model_architecture.py
@@ -0,0 +1,39 @@
+
+import torch
+from torch import nn
+
+import torch
+from torch import nn
+
+def conv(ni, nf, ks, stride=1, padding=1, act=True, bn=True, mp=True, dl=True):
+    layers = [nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, padding=padding)]
+    if bn:
+        layers.append(nn.BatchNorm2d(nf))
+    if act:
+        layers.append(nn.ReLU())
+    if mp:
+        layers.append(nn.MaxPool2d(2, 2))
+    if dl:
+        layers.append(nn.Dropout2d(0.25))
+    return nn.Sequential(*layers)
+
+def lin(inp, op, dl=True, act=True):
+    layers = [nn.Linear(inp, op)]
+    if dl:
+        layers.append(nn.Dropout(0.5))
+    if act:
+        layers.append(nn.ReLU())
+    return nn.Sequential(*layers)
+
+cnn_arch = nn.Sequential(
+    conv(3, 32, 3),
+    conv(32, 64, 3),
+    conv(64, 128, 3),
+    nn.Flatten(),  
+    lin(128 * 16 * 16, 256),
+    lin(256, 128),
+    lin(128, 15, act=False)
+)
+
+
+
