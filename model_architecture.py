e0e5b4d86698ae5d692d9424ba3bcff627bca76b
AmanK-tech <amankhatri216@gmail.com.com>
2025-01-27 15:16:32 +0000

    model_architecture

--- model_architecture.py
+++ model_architecture.py
@@ -2,45 +2,32 @@
 import torch
 from torch import nn
 
-def conv(ni, nf, ks, stride=1, padding=1, act=True, bn=True, mp=True, dl=True):
-    layers = [nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, padding=padding)]
-    if bn:
-        layers.append(nn.BatchNorm2d(nf))
-    if act:
-        layers.append(nn.ReLU())
-    if mp:
-        layers.append(nn.MaxPool2d(2, 2))
-    if dl:
-        layers.append(nn.Dropout2d(0.25))
-    return nn.Sequential(*layers)
+def conv(ni, nf, ks, stride=1, padding=1, act=True, bn=True, mp=True, dl=False):
 
-def lin(inp, op, dl=True, act=True):
-    layers = [nn.Linear(inp, op)]
-    if dl:
-        layers.append(nn.Dropout(0.5))
-    if act:
-        layers.append(nn.ReLU())
-    return nn.Sequential(*layers)
+  layers = [nn.Conv2d(ni, nf, kernel_size=ks, stride=stride, padding=padding)]
+  if bn: layers.append(nn.BatchNorm2d(nf))
+  if act: layers.append(nn.ReLU(inplace=True))  
+  if mp: layers.append(nn.MaxPool2d(2, 2))
+  return nn.Sequential(*layers)
 
 class CNNArchitecture(nn.Module):
-    def __init__(self, num_classes=15):
-        super().__init__()
-        self.features = nn.Sequential(
-            conv(3, 32, 3),
-            conv(32, 64, 3),
-            conv(64, 128, 3)
-        )
-        self.classifier = nn.Sequential(
-            nn.Flatten(),
-            lin(128 * 16 * 16, 256),
-            lin(256, 128),
-            lin(128, num_classes, act=False)
-        )
+  def __init__(self, num_classes=15):
+      super().__init__()
+      self.features = nn.Sequential(
+          conv(3, 32, 3),
+          conv(32, 64, 3),
+          conv(64, 128, 3),
+          nn.AdaptiveAvgPool2d((4, 4))  
+      )
+      self.classifier = nn.Sequential(
+          nn.Flatten(),
+          nn.Linear(128 * 4 * 4, 256),
+          nn.ReLU(inplace=True),
+          nn.Linear(256, num_classes)
+      )
 
-    def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
-        return x
+  def forward(self, x):
+      return self.classifier(self.features(x))
 
 cnn_arch = CNNArchitecture()
 
